53d164ed3c1bc2bc05461e102535c93f07a031ed
PhaniRahul873 <135304894+pahnirahul60@users.noreply.github.com>
2025-07-03 12:32:59 +0530

    LLD

---
+++ Design Patterns/Structural/Adapter/RazorpayGateway.java
@@ -0,0 +1,13 @@
+public class RazorpayGateway {
+
+    public String payByCreditCard(String creditCard,
+                                  String cvv,
+                                  String expiry) {
+        System.out.println("Payment Done by Razorpay");
+        return "123";
+    }
+
+    public boolean checkPaymentStatus(String id) {
+        return false;
+    }
+}
