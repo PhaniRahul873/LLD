233a9c71a51d4fe7c5a6d498da34fe264d3fea2a
PhaniRahul873 <135304894+pahnirahul60@users.noreply.github.com>
2025-07-07 15:23:10 +0530

    LLD

---
+++ LLD Problems/LoggingSystem/CORPattern/InfoLogger.java
@@ -0,0 +1,11 @@
+public class InfoLogger extends LogHandler {
+
+    public InfoLogger(int level, LogAppender appender) {
+        super(level, appender);
+    }
+
+    @Override
+    protected void write(String message) {
+        System.out.println("INFO: " + message);
+    }
+}
---
+++ LLD Problems/LoggingSystem/Strategies/LogAppender.java
@@ -0,0 +1,4 @@
+
+public interface LogAppender {
+    void append(LogMessage logMessage);
+}
